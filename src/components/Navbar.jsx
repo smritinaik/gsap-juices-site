d05022dab210e5466a0756360a4908f9aaa10c93
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-07-04 22:09:44 +0530

    app.jsx
    app.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div> 
+            {/* working on navbar */}
+            <a href='#home' className='flex items-center gap-2'>
+
+            </a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
